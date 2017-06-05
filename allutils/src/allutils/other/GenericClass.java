4e4a2ebdc1f3762b4dc378006f0e3a4dae9c3bc9
Administrator <Administrator@XB-20170405RMKL>
2017-06-05 16:11:10 +0800

    提交其他的工具类

---
+++ allutils/src/allutils/other/GenericClass.java
@@ -0,0 +1,20 @@
+package allutils.other;
+
+import java.lang.reflect.ParameterizedType;
+
+public class GenericClass {
+
+	/**
+	 * 获取父类的泛型类型
+	 * @param clazz
+	 * @return
+	 */
+	public static Class getGenericClass(Class clazz) {
+		ParameterizedType  type=(ParameterizedType)clazz.getGenericSuperclass();
+		
+		Class entityClass=(Class) type.getActualTypeArguments()[0];
+		
+		return entityClass;
+	}
+
+}
